c635a338fa61520697e3c501bae794e8a53eba66
chriswmann <8133496+chriswmann@users.noreply.github.com>
2024-11-23 20:38:46 +0000

    Add FileNotFound error

--- src/file.rs
+++ src/file.rs
@@ -1,12 +1,6 @@
 use std::fs;
 use std::path;
 
-pub fn load_file(file_path: &path::Path) -> Vec<String> {
-    fs::read_to_string(file_path)
-        .expect("Couldn't open path: {file_path}")
-        .lines()
-        .map(|l| l.to_owned())
-        .collect()
 pub fn load_file(file_path: &path::Path) -> Result<Vec<String>, crate::errors::CcsblError> {
     match fs::read_to_string(file_path) {
         Ok(content) => Ok(content.lines().map(|l| l.to_owned()).collect()),
