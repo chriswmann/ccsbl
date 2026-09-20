9486b09dbbd457ea702324d78b9aadb44b0bd116
Chris <8133496+chriswmann@users.noreply.github.com>
2026-09-20 16:35:49 +0100

    Validate the ccl file extension via clap
    Rather than 'manually' in main.rs.

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ use std::path;
 #[derive(Debug, Parser)]
 pub struct Cli {
     /// The file to compile or execute
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = validate_extension)]
     pub file: path::PathBuf,
 
     #[arg(short, long, default_value_t = false)]
@@ -12,4 +12,16 @@ pub struct Cli {
 
     #[arg(short, long, default_value_t = false)]
     pub execute: bool,
+}
+
+fn validate_extension(path: &str) -> Result<path::PathBuf, String> {
+    let path = path::PathBuf::from(path);
+    if path.extension().is_some_and(|ext| ext == "ccl") {
+        Ok(path)
+    } else {
+        Err(format!(
+            "File must have a .ccl extension: {}",
+            path.display()
+        ))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use file::load_file;
-use std::path;
 use std::process;
 
 use tracing::debug;
@@ -17,8 +16,7 @@ fn main() {
     let args = cli::Cli::parse();
     debug!("Args: {:#?}", &args);
 
-    let file_path = &args.file;
-    let lines = load_file(file_path.as_path());
+    let lines = load_file(args.file.clone());
     match &lines {
         Ok(_) => {}
         Err(e) => {
@@ -30,11 +28,6 @@ fn main() {
     let mut bytecode = vec![];
 
     if args.compile {
-        if path::Path::new(&args.file).extension().is_some()
-            && path::Path::new(&args.file).extension().unwrap() == "mclb"
-        {
-            eprintln!("Error: mclb file supplied, refusing to compile bytecode. Did you mean to execute it?");
-        }
         bytecode = match compiler::compile(&lines.unwrap()) {
             Err(err) => {
                 eprintln!("Error compiling '{}': {}", args.file.display(), err);
