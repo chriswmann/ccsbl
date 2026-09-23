b66b64568b9128de90765aebea926b5f8697522c
Chris <8133496+chriswmann@users.noreply.github.com>
2026-09-23 01:10:05 +0100

    Reserve 0x00 as invalid

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -7,26 +7,27 @@ use crate::errors::Error;
 #[derive(Clone, Copy, Debug, FromRepr, PartialEq)]
 #[repr(u8)]
 pub enum Op {
-    Push = 0x00, // ( -- a)
-    Pop = 0x01,  // (a -- )
-    Add = 0x02,  // (a b -- a+b)
-    Sub = 0x03,  // (a b -- a-b)
-    // Mul = 0x05,    // (a b -- a·b)
-    // Div = 0x05,    // (a b -- a÷b)
-    // Neg = 0x06,    // (a -- -a)
-    // Mod = 0x07,    // (-a -- a)
-    // Dup = 0x08,    // (a -- a a)
-    // Over = 0x09,   // (a b -- a b a)
-    // Swap = 0x0a,   // (a b -- b a)
-    // Rot = 0x0b,    // (a b c -- b c a)
-    // And = 0x0c,    // ( a b -- a & b )	Bitwise AND
-    // Or = 0x0d,     // ( a b -- a | b )	Bitwise OR
-    // Xor = 0x0e,    // ( a b -- a ^ b )	Bitwise exclusive OR
-    // Not = 0x0f,    // ( a -- ~a )	Bitwise complement
-    // LShift = 0x10, // ( a n -- a << n )	Shift a left by n bits
-    // RShift = 0x11, // ( a n -- a >> n )	Shift a right by n bits
-    Jmp = 0x12,   // (  -- )
-    Print = 0x14, // (a b -- a) with b printed to stdout
+    Push = 0x01, // ( -- a)
+    Pop = 0x02,  // (a -- )
+    Add = 0x03,  // (a b -- a+b)
+    Sub = 0x04,  // (a b -- a-b)
+    // Mul = 0x07,    // (a b -- a·b)
+    // Div = 0x07,    // (a b -- a÷b)
+    // Neg = 0x08,    // (a -- -a)
+    // Mod = 0x09,    // (-a -- a)
+    // Dup = 0x0a,    // (a -- a a)
+    // Over = 0x0b,   // (a b -- a b a)
+    // Swap = 0x0c,   // (a b -- b a)
+    // Rot = 0x0d,    // (a b c -- b c a)
+    // And = 0x0e,    // ( a b -- a & b )	Bitwise AND
+    // Or = 0x0f,     // ( a b -- a | b )	Bitwise OR
+    // Xor = 0x10,    // ( a b -- a ^ b )	Bitwise exclusive OR
+    // Not = 0x11,    // ( a -- ~a )	Bitwise complement
+    // LShift = 0x12, // ( a n -- a << n )	Shift a left by n bits
+    // RShift = 0x13, // ( a n -- a >> n )	Shift a right by n bits
+    Jmp = 0x14, // (  -- )
+    // Jt = 0x15  // ( -- )
+    Print = 0x19, // (a b -- a) with b printed to stdout
     Halt = 0xFF,  // end program
 }
 
@@ -158,7 +159,7 @@ mod tests {
 
     #[test]
     fn decode_returns_truncated_byte_error_when_bytes_are_truncated() {
-        let code = &[0x00, 1, 0];
+        let code = &[0x01, 1, 0];
         let offset = 0;
         match Instr::decode(code, offset) {
             Err(Error::TruncatedByteCode {
